6758264c8ecb46e4feb987c7e2a449e17384fb04
veldakarimi <veldakarimi@gmail.com>
2021-03-29 00:22:52 +0300

    feat: payment

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+class Payment(models.Model):
+        id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+        ref_number = models.CharField(max_length=300, blank=False)
