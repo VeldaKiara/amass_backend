03d6c642b1da9955e89806800295d6851acafc12
veldakarimi <veldakarimi@gmail.com>
2021-05-04 17:41:36 +0300

    adding ip

--- amass/settings.py
+++ amass/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '^-@dykue3(nfcazj9#fxszz@r!9&_a#chf-a)8=qdsmz_21+^e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '54.175.235.38']
 
 
 # Application definition
